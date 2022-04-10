232edd4f318b8eeea5d92dfc3343a5905a92c39c
Aaron Stannard <aaron@petabridge.com>
2022-04-10 10:42:59 -0500

    added backend, verified formation of cluster locally

---
+++ src/backend/AkkaDotNet.BackEnd/Program.cs
@@ -0,0 +1,30 @@
+using Akka.Actor;
+using Akka.Hosting;
+using AkkaDotNet.Infrastructure;
+using AkkaDotNet.Infrastructure.Actors;
+using AkkaDotNet.Infrastructure.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddEnvironmentVariables();
+
+var akkaConfiguration = builder.Configuration.GetRequiredSection(nameof(StressOptions)).Get<StressOptions>();
+
+builder.Services.AddAkka(ActorSystemConstants.ActorSystemName, configurationBuilder =>
+{
+    configurationBuilder.WithClusterBootstrap(akkaConfiguration.AkkaClusterOptions,
+        new[] { ActorSystemConstants.BackendRole, ActorSystemConstants.DistributedPubSubRole });
+    configurationBuilder.WithReadyCheckActors();
+});
+
+var app = builder.Build();
+
+app.MapGet("/ready", async (ActorRegistry registry) =>
+{
+    var readyCheck = registry.Get<ReadyCheckActor>();
+    var checkResult = await readyCheck.Ask<ReadyResult>(ReadyCheck.Instance, TimeSpan.FromSeconds(3));
+    if (checkResult.IsReady)
+        return Results.StatusCode(200);
+    return Results.StatusCode(500);
+});
+
+await app.RunAsync();
